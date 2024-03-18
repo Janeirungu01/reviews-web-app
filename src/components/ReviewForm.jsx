78806aa6eb4d11081aae6c32e01b1a6871ae94d8
Janeirungu01 <jwirungu96@gmail.com>
2024-03-18 05:36:48 +0300

    feat: set number of characters in input to 20

--- src/components/ReviewForm.jsx
+++ src/components/ReviewForm.jsx
@@ -1,9 +1,32 @@
 import { useState } from "react"
 import Button from "./Button"
 
+const errorStyle = {
+    display: 'block',
+    marginTop: '7px',
+    fontStyle:'italic'
+}
+
 function ReviewForm() {
+    //state for input text
     const [text, setText] = useState('')
+    //state for button disbale
+    const [btnDisabled, setBtnDisabled] = useState(true)
+    //state for validation (character)
+    const [msg, setMsg] = useState()
+    //function to update the text state(in the input field) and also validate the text characters to be above 20 before button will be enabled
     const textHandler =(e) => {
+        if(text === '') {
+            setBtnDisabled(true)
+            setMsg(null)
+        } else if(text !== '' && text.trim().length <=20){
+            setMsg('Your review must be more than 20 characters')
+            setBtnDisabled(true)
+        } else {
+            setMsg(null)
+            setBtnDisabled(false)
+        }
+
         setText(e.target.value)
     }
   return (
@@ -15,8 +38,9 @@ function ReviewForm() {
                 <div className="input-group">
                     <input type="text" value={text} placeholder="Write your review here" onChange={textHandler} />
                   
-                    <Button type='submit' variant='primary'>Submit</Button>
+                    <Button type='submit' variant='secondary' isDisabled={btnDisabled}>Submit</Button>
                 </div>
+                {msg && <span className="message" style={errorStyle}>{msg}</span>}
             </form>
             </div>
       </div>

93994aa03fc0b022e09dc3a5d836a7d46b6152e5
Janeirungu01 <jwirungu96@gmail.com>
2024-03-22 16:20:31 +0300

    feat: button reidrect to review page

--- src/App.jsx
+++ src/App.jsx
@@ -52,6 +52,7 @@ function App() {
         <Route path= '/' element={<Home />} /> 
         <Route path= '/about' element={<About />} /> 
         <Route path= '/review' element={<Review />} /> 
+        <Route path= '/home' element={<Home />} /> 
       </Routes>
     </Router>   
              
--- src/components/Pages/Home.jsx
+++ src/components/Pages/Home.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import Button from "../Button"
+import { useNavigate } from "react-router-dom"
 
 function Home() {
+  const navigate = useNavigate() 
+
   return (
     <>
-      <h1>Home</h1>
+    <div className="container"> 
+      <h1>Welcome to our Company</h1>
+      <p>Thankyou for using our product</p>
+      <p>We would like you to review about your experience with us.</p> 
+      <br /> <br />
+      <Button type='button' variant='secondary' onclick={() => navigate('/review')}>Review</Button>    
+     
+    </div>
+      
     </>
   )
 }
